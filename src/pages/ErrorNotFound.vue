3287f49b95ee78e5f2232c481c41fbb072834131
Ziegfrid Gualberto <ziegfrid.gualberto@gmail.com>
2024-06-16 08:24:19 +0800

    organize error page and add loading anition on slow network

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+      <div class="text-h4">404</div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+      <div class="text-h5" style="opacity: 0.4">Oops. Nothing here...</div>
 
       <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
+        class="q-mt-lg"
         unelevated
         to="/"
         label="Go Home"
         no-caps
+        outline
       />
     </div>
   </div>
@@ -24,6 +19,6 @@
 
 <script setup lang="ts">
 defineOptions({
-  name: 'ErrorNotFound'
+  name: 'ErrorNotFound',
 });
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { route } from 'quasar/wrappers';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import routes from './routes';
+import { Loading, QSpinnerFacebook } from 'quasar';
 
 /*
  * If not building with SSR mode, you can
@@ -17,6 +18,20 @@ export default route(function (/* { store, ssrContext } */) {
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
     history: createWebHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  Router.beforeEach((to, from, next) => {
+    Loading.show({
+      spinner: QSpinnerFacebook,
+      delay: 1000,
+      message: 'Network might be slow. Please wait...',
+    });
+
+    next();
+  });
+
+  Router.afterEach(() => {
+    Loading.hide();
   });
 
   return Router;
